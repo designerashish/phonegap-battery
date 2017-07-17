2d50ba1d615cf6578c5ec5a5e6ae23a12742bb0a
DesignerAshish <designerashish90@gmail.com>
2017-07-18 00:32:28 +0530

    Phonegap Battery
    Phonegap Battery

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,29 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-plugin-flashlight.Flashlight",
+        "file": "plugins/cordova-plugin-flashlight/www/Flashlight.js",
+        "pluginId": "cordova-plugin-flashlight",
+        "clobbers": [
+            "window.plugins.flashlight"
+        ]
+    },
+    {
+        "id": "cordova-plugin-battery-status.battery",
+        "file": "plugins/cordova-plugin-battery-status/www/battery.js",
+        "pluginId": "cordova-plugin-battery-status",
+        "clobbers": [
+            "navigator.battery"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.3.2",
+    "cordova-plugin-compat": "1.0.0",
+    "cordova-plugin-flashlight": "3.2.0",
+    "cordova-plugin-battery-status": "1.2.4"
+};
+// BOTTOM OF METADATA
+});
---
+++ platforms/browser/www/js/index-old.js
@@ -0,0 +1,82 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+    },
+
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+
+
+          var form = document.getElementById("myForm");
+          // ...and take over its submit event.
+            form.addEventListener("submit", function (event) {
+                event.preventDefault();
+                sendData();
+            });
+ }
+};
+
+
+
+
+
+
+
+ function sendData() {
+    var XHR = new XMLHttpRequest();
+
+    // Bind the FormData object and the form element
+    var FD = new FormData(form);
+
+    // Define what happens on successful data submission
+    XHR.addEventListener("load", function(event) {
+      form.reset();
+      //alert(event.target.responseText);
+      alert("User Data has been Saved.");
+     });
+
+    // Define what happens in case of error
+    XHR.addEventListener("error", function(event) {
+      alert('Oups! Something goes wrong.');
+    });
+
+    // Set up our request
+    XHR.open("POST", "http://designerashish.org/test.php");
+
+    // The data sent is what the user provided in the form
+    XHR.send(FD);
+  }
+
+
+
+
+
+app.initialize();
