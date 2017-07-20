f2ed359efd2c24d0b129ac65df5d5269b7454528
DesignerAshish <designerashish90@gmail.com>
2017-07-20 19:30:08 +0530

    comment

--- www/js/index.js
+++ www/js/index.js
@@ -80,7 +80,7 @@ function mscan(){
           saveHistory: true, // Android, save scan history (default false)
           prompt : "Place a barcode inside the scan area", // Android
           resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
-          formats : "default, QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+          formats : "default,QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
           orientation : "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
           disableAnimations : true, // iOS
           disableSuccessBeep: false // iOS
